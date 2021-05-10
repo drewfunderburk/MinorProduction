731a3e6db2c9339a9eb89a26c78f49cf876b587e
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-05-10 14:46:18 -0500

    chore: Added gizmo path to PursueTargetBehaviour

--- Assets/Scripts/AI/PursueTargetBehaviour.cs
+++ Assets/Scripts/AI/PursueTargetBehaviour.cs
@@ -45,6 +45,27 @@ public class PursueTargetBehaviour : MonoBehaviour
         // Update NavMeshAgent's destination
         _agent.SetDestination(Target.position);
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!enabled)
+            return;
+
+        // If the agent has a path
+        if (_agent.hasPath)
+        {
+            // Draw a line between each of the path corners
+            Gizmos.color = Color.green;
+            Vector3[] path = _agent.path.corners;
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (i == 0)
+                    Gizmos.DrawLine(transform.position, path[i]);
+                else
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+            }
+        }
+    }
 }
 
 
