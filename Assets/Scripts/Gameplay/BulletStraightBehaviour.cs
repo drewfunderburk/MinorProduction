354a85789ea5d4296a23f6b067494e5740fab005
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-05-10 16:55:25 -0500

    chore: Abstracted bullet code to BulletBehaviour

--- Assets/Scripts/Gameplay/BulletStraightBehaviour.cs
+++ Assets/Scripts/Gameplay/BulletStraightBehaviour.cs
@@ -3,32 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class BulletStraightBehaviour : MonoBehaviour
+public class BulletStraightBehaviour : BulletBehaviour
 {
-    [SerializeField] private float _speed = 10f;
-    public float Speed { get; set; }
-
-    [SerializeField] private float _despawnTime = 5f;
-    public float DespawnTime { get; set; }
-
-    [SerializeField] private LayerMask _collideWith = ~0;
-
     private Rigidbody _rigidbody;
 
     private void Start()
     {
         // Give this object its initial velocity
         _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.velocity = transform.forward * _speed;
+        _rigidbody.velocity = transform.forward * Speed;
 
         // Destroy this object after it's time has expired
-        Destroy(this.gameObject, _despawnTime);
+        Destroy(this.gameObject, DespawnTime);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         // If the collided object's layer is in _collideWith, destroy this object
-        if (((1 << collision.gameObject.layer) & _collideWith) != 0)
+        if (((1 << collision.gameObject.layer) & CollideWith) != 0)
             Destroy(this.gameObject);
     }
 }
