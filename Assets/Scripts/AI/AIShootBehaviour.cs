89ef66f5b9e1ea1aa41ab6deb5507ca8d9f4bcc4
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-05-14 08:54:00 -0500

    fix: AIShootBehaviour fire locations follow object properly

--- Assets/Scripts/AI/AIShootBehaviour.cs
+++ Assets/Scripts/AI/AIShootBehaviour.cs
@@ -13,33 +13,34 @@ public class AIShootBehaviour : MonoBehaviour
     [SerializeField] private GameObject _bulletPrefab = null;
 
     [Tooltip("Where to fire the projectile from")]
-    [SerializeField] private Vector3[] _fireLocations = null;
-    public Vector3[] FireLocations { get { return _fireLocations; } }
-
+    [SerializeField] private Vector3[] _fireLocationOffsets = null;
+    public Vector3[] FireLocationOffsets { get { return _fireLocationOffsets; } set { _fireLocationOffsets = value; } }
 
     [Tooltip("Delay in seconds between shots")]
     [SerializeField] private float _fireDelay = 1f;
 
     private float _fireTimer = 0;
 
-    private Vector3[] _fireLocationsWithOffset = null;
-    public Vector3[] FireLocationsWithOffset { get => _fireLocationsWithOffset; private set => _fireLocationsWithOffset = value; }
+    private Vector3[] _fireLocations = null;
+    public Vector3[] FireLocations { get => _fireLocations; private set => _fireLocations = value; }
 
-    private void UpdateFireLocationsWithOffset()
+    public void UpdateFireLocations()
     {
-        for (int i = 0; i < FireLocationsWithOffset.Length; i++)
+        FireLocations = new Vector3[FireLocationOffsets.Length];
+
+        for (int i = 0; i < FireLocations.Length; i++)
         {
             // Get relative offset position
-            Vector3 x = transform.right * FireLocations[i].x;
-            Vector3 y = transform.up * FireLocations[i].y;
-            Vector3 z = transform.forward * FireLocations[i].z;
-            FireLocationsWithOffset[i] = x + y + z;
+            Vector3 x = transform.right * FireLocationOffsets[i].x;
+            Vector3 y = transform.up * FireLocationOffsets[i].y;
+            Vector3 z = transform.forward * FireLocationOffsets[i].z;
+            FireLocations[i] = x + y + z;
         }
     }
 
     private void Start()
     {
-        FireLocationsWithOffset = new Vector3[_fireLocations.Length];
+        UpdateFireLocations();
     }
 
     private void Update()
@@ -47,17 +48,15 @@ public class AIShootBehaviour : MonoBehaviour
         // Increment timer
         _fireTimer += Time.deltaTime;
 
-        UpdateFireLocationsWithOffset();
-
         // If time has passed for a new shot
         if (_fireTimer > _fireDelay)
         {
             // Reset timer
             _fireTimer = 0;
 
-            for (int i = 0; i < FireLocationsWithOffset.Length; i++)
+            for (int i = 0; i < FireLocations.Length; i++)
             {
-                GameObject bullet = Instantiate(_bulletPrefab, FireLocationsWithOffset[i], Quaternion.identity);
+                GameObject bullet = Instantiate(_bulletPrefab, FireLocations[i], Quaternion.identity);
 
                 if (Target != null)
                     bullet.transform.LookAt(Target);
@@ -69,36 +68,17 @@ public class AIShootBehaviour : MonoBehaviour
     {
         if (!enabled)
             return;
+        if (FireLocations == null)
+            return;
+
+        UpdateFireLocations();
 
         // Draw wire spheres at each of the Fire Locations
-        for (int i = 0; i < _fireLocations.Length; i++)
+        for (int i = 0; i < FireLocations.Length; i++)
         {
             Gizmos.color = Color.green;
-            Handles.Label(FireLocationsWithOffset[i], "Fire location " + i);
-            Gizmos.DrawWireSphere(FireLocationsWithOffset[i], 0.2f);
-        }
-    }
-}
-
-[CustomEditor(typeof(AIShootBehaviour))]
-public class AIShootBehaviourEditor : Editor
-{
-    private void OnSceneGUI()
-    {
-        AIShootBehaviour script = target as AIShootBehaviour;
-
-        if (!script.enabled)
-            return;
-        if (script.FireLocations == null)
-            return;
-
-        // Create position handles for each of the Fire Locations to allow them to be moved in the scene view
-        for (int i = 0; i < script.FireLocationsWithOffset.Length; i++)
-        {
-            EditorGUI.BeginChangeCheck();
-            Vector3 newPosition = Handles.PositionHandle(script.FireLocationsWithOffset[i], Quaternion.identity);
-            if (EditorGUI.EndChangeCheck())
-                script.FireLocations[i] = (newPosition - script.transform.position);
+            Handles.Label(FireLocations[i], "Fire location " + i);
+            Gizmos.DrawWireSphere(FireLocations[i], 0.2f);
         }
     }
 }
