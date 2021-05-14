8bf4a4c5cfbd8df843c682fb8a00e8ca0b9851d6
Seth Daigle <laflyboy@live.com>
2021-05-14 08:14:54 -0600

    Added Planet, Player Ship, and Warping
    Upgraded to URP, added shadergraph, added planet shader, added planet randomizer script, added camera lerp-to-pos script, added ship warp movement script, added warp particles

---
+++ Assets/Scripts/WarpDriveMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpDriveMovement : MonoBehaviour
+{
+    public Vector2 MinMax;
+    public float speed;
+
+    public float timerX = 0f;
+    public float timerY = 0f;
+
+    public bool isWarp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("f"))
+        {
+            isWarp = !isWarp;
+        }
+
+        if(isWarp == true)
+        {
+            warpMovement();
+        }
+        else { timerX = 0f; }
+    }
+
+    public void warpMovement()
+    {
+        gameObject.transform.position = new Vector3(Mathf.Lerp(MinMax.x, MinMax.y, (Mathf.Sin(timerX)/2)+0.5f), Mathf.Lerp(MinMax.x/2f, MinMax.y/2f, (Mathf.Sin(timerY) / 2) + 0.5f), 0);
+        //gameObject.transform.rotation = new Quaternion(0, 0, Mathf.Lerp(0, 90, (Mathf.Sin(timerX) / 2) + 0.5f), gameObject.transform.rotation.w);
+        timerX += speed * Time.deltaTime;
+        timerY = timerX * 1.25f;
+    }
+
+}
