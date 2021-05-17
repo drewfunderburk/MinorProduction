3e9cd13465ddd502407f81cbb92558c68effdff9
Josiah Hartley <mrred.hartley@gmail.com>
2021-05-17 16:49:30 -0500

    Banking Added But Glitchy, Only works one way

--- Assets/Scripts/Gameplay/Player/PlayerMovementBehaviour.cs
+++ Assets/Scripts/Gameplay/Player/PlayerMovementBehaviour.cs
@@ -6,28 +6,42 @@ public class PlayerMovementBehaviour : CombatActor
 {
     private Rigidbody _rigidbody;
     private Vector3 _velocity;
+    [Tooltip("The Speed at Which The Player Reaches the Desired Rotation During Movement")]
     [SerializeField]
     private float _bankingSpeed = 1;
+    [Tooltip("The Speed at Which The Player Will Move")]
     [SerializeField]
     private float _moveSpeed = 1;
-    private float _currentRotation;
+    [Tooltip("The Final Rotation the Player Will Reach During Movement in Degrees")]
+    [SerializeField]
+    private Vector3 _desiredRotation;
+    private Vector3 _currentRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _currentRotation = _rigidbody.rotation.z;
+        _currentRotation = _rigidbody.rotation.eulerAngles;
     }
 
     public void Move(Vector3 direction)
     {
         _velocity = direction * _moveSpeed * Time.deltaTime;
-        _rigidbody.rotation.z += (direction.x / Mathf.Abs(direction.x));
+        Vector3 Rotation = Vector3.Lerp(_currentRotation, _desiredRotation, 1 * _bankingSpeed);
+
+        if (direction.x > 0 && _currentRotation.z < _desiredRotation.z)
+            transform.Rotate(-Rotation);
+        else if (direction.x < 0 && _currentRotation.z < _desiredRotation.z)
+            transform.Rotate(Rotation);
+
+
+        _currentRotation = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+                                                  //swap x and y because player does not move on y axis
         _rigidbody.MovePosition(transform.position + new Vector3(_velocity.x,_velocity.z,_velocity.y));
     }
 
