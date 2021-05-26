1a15a8c89ea1ee5d5c38ad17f16ece32889de46b
Seth Daigle <laflyboy@live.com>
2021-05-26 16:53:47 -0500

    feat: Planet movement script

---
+++ Assets/Scripts/Warping/CameraMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Vector3 iniPos;
+    private Vector3 warpPos;
+    private Quaternion iniRot;
+    private Quaternion warpRot;
+    private float lerpTime = 0f;
+    private float iniFOV;
+    private bool isWarping = false;
+
+    public float enRouteFOV = 30f;
+    public float lerpSpeed = 1f;
+    public Transform enRouteTransform;
+    public ParticleSystem[] warpEffect;
+    public ParticleSystem spoolUpParticle;
+
+    //PLANET TEST
+    public GameObject activePlanet;
+
+
+    void Start()
+    {
+        iniPos = gameObject.transform.position;
+        iniRot = gameObject.transform.rotation;
+
+        warpPos = enRouteTransform.transform.position;
+        warpRot = enRouteTransform.transform.rotation;
+        if (GetComponentInChildren<Camera>())
+        {
+            iniFOV = GetComponentInChildren<Camera>().fieldOfView;
+        }
+
+    }
+
+    void Update()
+    {
+        if(lerpTime >= 1f)
+        {
+            activePlanet.GetComponent<PlanetBehavior>().Generate();
+        }
+
+        if (Input.GetKeyDown("f"))
+        {
+            ToggleWarp();
+        }
+
+        if (isWarping == true)
+        {
+            Mathf.Clamp01(lerpTime = (lerpTime < 1f ? lerpTime + Time.deltaTime * lerpSpeed : 1f));
+
+            foreach (ParticleSystem particle in warpEffect)
+            {
+                if (!particle.isPlaying)
+                {
+                    particle.Play();
+                }
+            }
+
+        }
+        else 
+        {
+            Mathf.Clamp01(lerpTime = (lerpTime > 0f ? lerpTime - Time.deltaTime * lerpSpeed :  0f));
+
+            foreach (ParticleSystem particle in warpEffect)
+            {
+                if (particle.isPlaying)
+                {
+                    particle.Stop();
+                }
+            }
+        }
+
+        if (GetComponentInChildren<Camera>())
+        {
+            GetComponentInChildren<Camera>().fieldOfView = Mathf.SmoothStep(iniFOV, enRouteFOV, lerpTime);
+        }
+
+        if (lerpTime < 1f || lerpTime > 0)
+        {
+            transform.position = new Vector3(Mathf.SmoothStep(iniPos.x, warpPos.x, lerpTime), Mathf.SmoothStep(iniPos.y, warpPos.y, lerpTime), Mathf.SmoothStep(iniPos.z, warpPos.z, lerpTime));
+            transform.rotation = new Quaternion(Mathf.SmoothStep(iniRot.x, warpRot.x, lerpTime), Mathf.SmoothStep(iniRot.y, warpRot.y, lerpTime), Mathf.SmoothStep(iniRot.z, warpRot.z, lerpTime), Mathf.SmoothStep(iniRot.w, warpRot.w, lerpTime));
+        }
+    }
+
+    void SpoolUp()
+    {
+        if (!spoolUpParticle.isPlaying)
+        {
+            spoolUpParticle.Play();
+        }
+    }
+
+    void ToggleWarp()
+    {
+        isWarping = !isWarping;
+        if(isWarping == true) { SpoolUp(); }
+    }
+
+}
---
+++ Assets/Scripts/Warping/PlanetMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMovement : MonoBehaviour
+{
+    public Vector3 farPos;
+    [Range(0, 1)] public float travelTime;
+    public Vector3 startPos;
+    public Vector3 endPos;
+    [Range(0, 1)] public float planetPosition;
+
+    public bool levelStatus = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelStatus == true)
+        {
+            gameObject.transform.position = new Vector3(Mathf.SmoothStep(startPos.x, endPos.x, planetPosition), Mathf.SmoothStep(startPos.y, endPos.y, planetPosition), Mathf.SmoothStep(startPos.z, endPos.z, planetPosition));
+        }
+        else { planetEnRoute(); }
+    }
+
+    public void planetEnRoute()
+    {
+        gameObject.transform.position = new Vector3(Mathf.SmoothStep(farPos.x, startPos.x, travelTime), Mathf.SmoothStep(farPos.y, startPos.y, travelTime), Mathf.SmoothStep(farPos.z, startPos.z, travelTime));
+    }
+
+}
