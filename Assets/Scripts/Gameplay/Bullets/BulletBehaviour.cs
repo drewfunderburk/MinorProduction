95981d99dcfb5a23e8d0eff29d55823054823756
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-06-02 16:52:11 -0500

    fix: Changed bullets to use Trigger colliders

--- Assets/Scripts/Gameplay/Bullets/BulletBehaviour.cs
+++ Assets/Scripts/Gameplay/Bullets/BulletBehaviour.cs
@@ -11,8 +11,4 @@ public abstract class BulletBehaviour : MonoBehaviour
     [Tooltip("How long this bullet will stay in the scene before destroying itself")]
     [SerializeField] private float _despawnTime;
     public float DespawnTime { get => _despawnTime; set => _despawnTime = value; }
-
-    [Tooltip("What layers this bullet should collide with")]
-    [SerializeField] private LayerMask _collideWith = ~0;
-    public LayerMask CollideWith { get => _collideWith; set => _collideWith = value; }
 }
