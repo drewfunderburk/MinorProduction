24a79e495c7f891dafb9a7ff4cc64d2f76a56e48
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-06-02 15:17:39 -0500

    feat: Power Up Movement Behaviour

---
+++ Assets/Scripts/Gameplay/PowerUps/PowerUpMovementBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PowerUpMovementBehaviour : MonoBehaviour
+{
+    [Tooltip("How fast this powerup should move")]
+    [SerializeField] private float _speed = 5;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(transform.position + (-Vector3.forward * _speed * Time.deltaTime));
+    }
+}
