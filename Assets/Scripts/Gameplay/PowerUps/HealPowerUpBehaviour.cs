96a87ae5afce901b547a80f8be3b353d4771061a
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-06-02 12:57:14 -0500

    feat: HealPowerUp

---
+++ Assets/Scripts/Gameplay/PowerUps/HealPowerUpBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class HealPowerUpBehaviour : MonoBehaviour, IPowerUp
+{
+    [SerializeField] private float _amountToHeal = 0;
+
+    public bool ApplyPowerUp(GameObject obj)
+    {
+        // Attempt to get a PlayerHealthBehaviour from obj
+        PlayerHealthBehaviour health = obj.GetComponent<PlayerHealthBehaviour>();
+
+        // If we were successful, regen health and return true
+        if (health)
+        {
+            health.RegenHealth(_amountToHeal);
+            return true;
+        }
+        else
+        {
+            // Otherwise log an error and return false
+            Debug.LogError("Could not find a PlayerHealthBehaviour");
+            return false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If the power up was applied, destroy this power up
+        if (ApplyPowerUp(other.gameObject))
+            Destroy(this.gameObject);
+    }
+}
