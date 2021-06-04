cd0451e146a6a7f132559299ebe413bfbfa880af
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-06-04 11:47:48 -0500

    feat: ShipSpeedIncrease Power Up

---
+++ Assets/Scripts/Gameplay/PowerUps/ShipSpeedPowerUpBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSpeedPowerUpBehaviour : MonoBehaviour, IPowerUp
+{
+    private enum ChangeType
+    {
+        ADDITIVE,
+        MULTIPLICATIVE
+    }
+
+    [Tooltip("How to apply the speed change. Additive to simply add or subtract the value, Multiplicative to multiply the current speed by the value")]
+    [SerializeField] private ChangeType _changeType = ChangeType.MULTIPLICATIVE;
+    [SerializeField] private float _speedChange = 1.2f;
+
+
+    public bool ApplyPowerUp(GameObject obj)
+    {
+        // Ensure obj has a player movement behaviour and get a reference to it
+        PlayerMovementBehaviour movement = obj.GetComponent<PlayerMovementBehaviour>();
+        if (movement == null)
+            return false;
+
+        switch (_changeType)
+        {
+            // If the type is additive, add the speed change
+            case ChangeType.ADDITIVE:
+                movement.MoveSpeed += _speedChange;
+                return true;
+            // If the type is percentile, multiply the speed change
+            case ChangeType.MULTIPLICATIVE:
+                movement.MoveSpeed *= _speedChange;
+                return true;
+            // If somehow neither is true, return false
+            default:
+                return false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If the power up was applied, destroy this power up
+        if (ApplyPowerUp(other.gameObject))
+            Destroy(this.gameObject);
+    }
+}
