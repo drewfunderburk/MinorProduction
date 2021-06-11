644e6d00b4b7ba4d303b5aca17749c73f4364983
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-06-11 09:19:48 -0500

    feat: Added warp timer to GameManager and attached to OnWarpEnter

--- Assets/Scripts/Gameplay/GameManagerBehaviour.cs
+++ Assets/Scripts/Gameplay/GameManagerBehaviour.cs
@@ -44,6 +44,9 @@ public class GameManagerBehaviour : MonoBehaviour
     [Tooltip("Level duration in seconds")]
     [SerializeField] private float _levelDuration = 30;
 
+    [Tooltip("Duration of warp animation")]
+    [SerializeField] private float _warpDuration = 5;
+
 
     // Events
     [Space]
@@ -54,10 +57,9 @@ public class GameManagerBehaviour : MonoBehaviour
     public UnityEvent OnWarpEnter;
     public UnityEvent OnWarpExit;
 
-    private bool _isGameOver = false;
+    private float _warpTimer = 0;
 
-    public bool IsGameOver { get { return _isGameOver; } }
-
+    #region PROPERTIES
     /// <summary>
     /// Game's current state. Changing this will invoke OnEnter and OnExit events for the state left and the state entered
     /// </summary>
@@ -120,6 +122,9 @@ public class GameManagerBehaviour : MonoBehaviour
     public float LevelDuration { get => _levelDuration; set => _levelDuration = value; }
     public int EasyPlanetLevelIncrease { get => _easyPlanetLevelIncrease; set => _easyPlanetLevelIncrease = value; }
     public int HardPlanetLevelIncrease { get => _hardPlanetLevelIncrease; set => _hardPlanetLevelIncrease = value; }
+    public float WarpDuration { get => _warpDuration; set => _warpDuration = value; }
+
+    #endregion
 
     private void Awake()
     {
@@ -138,20 +143,53 @@ public class GameManagerBehaviour : MonoBehaviour
 
         // Clamp level to bounds
         _level = Mathf.Clamp(_level, 0, _maxLevel);
+
+        // Add warp timer to OnWarpEnter event
+        OnWarpEnter.AddListener(() => StartCoroutine(WarpTimer()));
     }
 
+    /// <summary>
+    /// Increases the score by the given value
+    /// </summary>
     public void IncreaseScore(int score)
     {
-        _score += score;
+        if (score > 0)
+            _score += score;
     }
 
+    /// <summary>
+    /// Increase the level by _easyPlanetLevelIncrease
+    /// </summary>
     public void IncreaseLevelEasy()
     {
         _level += _easyPlanetLevelIncrease;
     }
 
+    /// <summary>
+    /// Increase the level by _hardPlanetLevelIncrease
+    /// </summary>
     public void IncreaseLevelHard()
     {
         _level += _hardPlanetLevelIncrease;
+    }
+
+    /// <summary>
+    /// Coroutine for handling the warp timer
+    /// </summary>
+    private IEnumerator WarpTimer()
+    {
+        // While timer has not reached duration
+        while(_warpTimer <= _warpDuration)
+        {
+            // Increment timer
+            _warpTimer += Time.deltaTime;
+
+            // Wait for next frame
+            yield return null;
+        }
+
+        // Set gamestate to GAME and fire events
+        if (GameState != GameStates.GAME)
+            GameState = GameStates.GAME;
     }
 }
